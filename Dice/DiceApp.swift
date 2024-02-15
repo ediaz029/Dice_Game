b3e660ad62d5421727a4133e5cafbf34726e89b7
ediaz029 <ediazj03@gmail.com>
2024-02-15 16:53:30 -0600

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Ernesto Diaz on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
