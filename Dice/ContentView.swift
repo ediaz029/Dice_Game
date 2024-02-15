26f82d3036b20b97f64a2abc0742c7563a5ce34f
ediaz029 <ediazj03@gmail.com>
2024-02-15 17:35:34 -0600

    Added existing project files to git

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Dice
+//  Dice_Game
 //
 //  Created by Ernesto Diaz on 2/15/24.
 //
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var diceNumber = 1
+    @State var diceNumber2 = 1
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            VStack {
+                Spacer()
+                Text("Ernesto Diaz").font(.largeTitle).foregroundStyle(Color.black)
+                Spacer()
+                Text("BEST Dice Game")
+                Spacer()
+                Image("cooldice").resizable()
+                    .scaledToFit().padding()
+                Spacer()
+                HStack {
+                    DiceView(n: diceNumber)
+                    DiceView(n: diceNumber2)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.diceNumber = Int.random(in: 1...6)
+                    self.diceNumber2 = Int.random(in: 1...6)
+                   
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                    
+                }
+                .background(Color.black)
+                Text("")
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
